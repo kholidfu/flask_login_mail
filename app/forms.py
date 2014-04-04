18c11fef9d76900e6e49c574b0161cf281981ac1
sopier <sopier@gmail.com>
2014-04-04 17:05:16 +0700

    complete!

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,9 @@
 from flask_wtf import Form
-from wtforms import TextField, TextAreaField, SubmitField, validators, ValidationError
+from wtforms import TextField, TextAreaField, SubmitField, validators, ValidationError, PasswordField
+import pymongo
+
+c = pymongo.Connection()
+db = c['users']
 
 class ContactForm(Form):
     name = TextField("Name",
@@ -12,3 +16,42 @@ class ContactForm(Form):
     message = TextAreaField("Message",
                             [validators.Required("Please enter a message.")])
     submit = SubmitField("Send")
+
+class SignupForm(Form):
+    firstname = TextField("First name", [validators.Required("Please enter your first name.")])
+    lastname = TextField("Last name", [validators.Required("Please enter your last name.")])
+    email = TextField("Email", [validators.Required("Please enter your email.")])
+    password = PasswordField("Password", [validators.Required("Please enter your password.")])
+    submit = SubmitField("Create Account")
+
+    def __init__(self, *args, **kwargs):
+        Form.__init__(self, *args, **kwargs)
+
+    def validate(self):
+        if not Form.validate(self):
+            return False
+        user = db.user.find_one({"email": self.email.data.lower()})
+        if user:
+            self.email.errors.append("That email is already taken.")
+            return False
+        else:
+            return True
+
+class SigninForm(Form):
+    email = TextField("Email", [validators.Required("Please enter your email.")])
+    password = PasswordField("Password", [validators.Required("Please enter your password.")])
+    submit = SubmitField("Sign in")
+
+    def __init__(self, *args, **kwargs):
+        Form.__init__(self, *args, **kwargs)
+
+    def validate(self):
+        if not Form.validate(self):
+            return False
+
+        user = db.user.find_one({"email": self.email.data.lower()})
+        if user and user['password'] == self.password.data:
+            return True
+        else:
+            self.email.errors.append("Invalid email or password")
+            return False
--- app/views.py
+++ app/views.py
@@ -1,10 +1,15 @@
 # author: @sopier
-from flask import render_template, request, redirect, send_from_directory
+from flask import render_template, request, redirect, send_from_directory, session, url_for
 from flask import make_response # untuk sitemap
 from flask import flash
 from flask.ext.mail import Message, Mail
 from app import app
-from forms import ContactForm
+from forms import ContactForm, SignupForm, SigninForm
+import pymongo
+
+c = pymongo.Connection()
+db = c['users']
+
 
 # build mail object
 mail = Mail()
@@ -28,3 +33,57 @@ def contact():
             return render_template("contact.html", success=True)
     elif request.method == "GET":
         return render_template("contact.html", form=form)
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    form  = SignupForm()
+
+    if request.method == "POST":
+        if form.validate() == False:
+            return render_template("signup.html", form=form)
+        else:
+            db.user.insert({
+                "firsname": form.firstname.data,
+                "lastname": form.lastname.data,
+                "email": form.email.data,
+                "password": form.password.data,
+                })
+            session['email'] = form.email.data
+            return redirect(url_for('profile'))
+
+    elif request.method == "GET":
+        return render_template("signup.html", form=form)
+
+@app.route("/profile")
+def profile():
+    if "email" not in session:
+        return redirect(url_for("signin"))
+
+    user = db.user.find_one({"email": session["email"]})
+
+    if user is None:
+        return redirect(url_for("signin"))
+    else:
+        return render_template("profile.html")
+
+@app.route("/signin", methods=["GET", "POST"])
+def signin():
+    form = SigninForm()
+
+    if request.method == "POST":
+        if form.validate() == False:
+            return render_template("signin.html", form=form)
+        else:
+            session['email'] = form.email.data
+            return redirect(url_for('profile'))
+
+    elif request.method == "GET":
+        return render_template("signin.html", form=form)
+
+@app.route("/signout")
+def signout():
+    if "email" not in session:
+        return redirect(url_for("signin"))
+
+    session.pop("email", None)
+    return redirect(url_for("signup"))
