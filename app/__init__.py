e344ecb319b9a527a41e7aa2027e9f0e3b71ba39
sopier <sopier@gmail.com>
2014-04-04 11:24:13 +0700

    edit password

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,7 @@ app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 465
 app.config["MAIL_USE_SSL"] = True
 app.config["MAIL_USERNAME"] = "sopier@gmail.com"
-app.config["MAIL_PASSWORD"] = "gm41lv3rt1g0"
+app.config["MAIL_PASSWORD"] = "somepassword"
 
 mail.init_app(app)
 
