cf6161c1ea68c10be277c5b7b91f759ae889467f
sopier <sopier@gmail.com>
2014-04-04 11:29:05 +0700

    tidy up the code

--- app/views.py
+++ app/views.py
@@ -1,18 +1,12 @@
 # author: @sopier
-
 from flask import render_template, request, redirect, send_from_directory
 from flask import make_response # untuk sitemap
-from app import app
-# untuk find_one based on data id => db.freewaredata.find_one({'_id': ObjectId(file_id)})
-# atom feed
-from werkzeug.contrib.atom import AtomFeed
-from bson.objectid import ObjectId
-from filters import slugify, splitter, onlychars, get_first_part, get_last_part, formattime, cleanurl
-from forms import ContactForm
 from flask import flash
-import datetime
 from flask.ext.mail import Message, Mail
+from app import app
+from forms import ContactForm
 
+# build mail object
 mail = Mail()
 
 @app.route("/contact", methods=["GET", "POST"])
@@ -23,8 +17,7 @@ def contact():
         if form.validate() == False:
             flash("All fields are required.")
             return render_template("contact.html", form=form)
-        else:
-            # all form fields validated
+        else: # all form fields validated
             msg = Message(form.subject.data, sender="sopier@gmail.com", recipients=["kh.fuadi@gmail.com"])
             msg.body = """
             From: %s <%s>
